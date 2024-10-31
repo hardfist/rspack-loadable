d4b66e0acaa1aedf4b7e2197a7fa8d653b9bc0b8
yangjian.fe <yangjian.fe@bytedance.com>
2024-10-31 22:54:22 +0800

    chore: use swc

--- rspack.config.js
+++ rspack.config.js
@@ -1,67 +1,73 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const LoadablePlugin = require('@loadable/webpack-plugin');
-const { javascript } = require('webpack');
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const LoadablePlugin = require("@loadable/webpack-plugin");
+const { javascript } = require("webpack");
 
-
-const DIST_PATH = path.resolve(__dirname, 'public/dist')
-const production = process.env.NODE_ENV === 'production'
-const development = !production
+const DIST_PATH = path.resolve(__dirname, "public/dist");
+const production = process.env.NODE_ENV === "production";
+const development = !production;
 /**
- * 
- * @param {*} target 
- * @returns {import('@babel/core').ConfigFunction}
+ *
+ * @param {*} target
+ * @returns {import('@rspack/core').ConfigFunction}
  */
-const getConfig = target => ({
+const getConfig = (target) => ({
   context: __dirname,
-  devtool:false,
+  devtool: false,
   name: target,
-  mode: development ? 'development' : 'production',
+  mode: development ? "development" : "production",
   target,
   entry: `./src/client/main-${target}.js`,
   module: {
-    parser: {
-      javascript: {
-        
-      }
-    },
     rules: [
       {
         test: /\.js?$/,
         exclude: /node_modules/,
         use: {
-          loader: require.resolve('babel-loader'),
+          loader: "builtin:swc-loader",
+          /**
+           * @type {import('@rspack/core').SwcLoaderOptions}
+           */
           options: {
-            configFile: path.resolve(__dirname,'./babel.config.js'),
-            caller: { target },
+            jsc: {
+              parser: {
+                syntax: "ecmascript",
+                jsx: true,
+              },
+              experimental: {
+                plugins: [["@swc/plugin-loadable-components",{}]],
+              },
+              transform: {
+                react: {
+                  runtime: "automatic",
+                },
+              },
+            },
           },
         },
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   externals:
-    target === 'node' ? ['@loadable/component', nodeExternals()] : undefined,
+    target === "node" ? ["@loadable/component", nodeExternals()] : undefined,
 
   optimization: {
     // this will lead to runtime error
-    runtimeChunk: target !== 'node',
-    minimize:false
+    runtimeChunk: target !== "node",
+    minimize: false,
   },
 
   output: {
     path: path.join(DIST_PATH, target),
-    filename: production ? '[name]-bundle-[chunkhash:8].js' : '[name].js',
+    filename: production ? "[name]-bundle-[chunkhash:8].js" : "[name].js",
     publicPath: `/dist/${target}/`,
-    libraryTarget: target === 'node' ? 'commonjs2' : undefined,
+    libraryTarget: target === "node" ? "commonjs2" : undefined,
   },
   plugins: [new LoadablePlugin()],
-})
+});
 
-module.exports = [getConfig('web'), getConfig('node')]
+module.exports = [getConfig("web"), getConfig("node")];
